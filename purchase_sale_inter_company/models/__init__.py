82c9c02561a1e155b48894100a884f28a3ab850d
Miquel Raïch <miquel.raich@forgeflow.com>
2023-09-06 13:13:28 +0200

    [FIX] purchase_sale_inter_company: consider moves that contain same products
    Imagine you have a customization that enables the split of a stock move into several moves (distributed in several pickings). Thus, we need to handle this case.

--- purchase_sale_inter_company/models/__init__.py
+++ purchase_sale_inter_company/models/__init__.py
@@ -3,3 +3,8 @@ from . import purchase_order
 from . import res_company
 from . import res_config
 from . import sale_order
+<<<<<<< HEAD
+=======
+from . import stock_picking
+from . import stock_move
+>>>>>>> 5026d8f7 ([FIX] purchase_sale_inter_company: consider moves that contain same products)
