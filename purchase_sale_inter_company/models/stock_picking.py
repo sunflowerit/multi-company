2deb431a3c01ca19c52c5a61f670a6a061b082d1
ntsirintanis <ntsirintanis@therp.nl>
2024-09-11 15:44:30 +0200

    fixup! fixup! [UPD] purchase_sale_inter_company: notify appropriate user

--- purchase_sale_inter_company/models/stock_picking.py
+++ purchase_sale_inter_company/models/stock_picking.py
@@ -151,11 +151,11 @@ class StockPicking(models.Model):
                 for ml, po_ml in zip(move_lines, po_move_lines):
                     pick._sync_lots(ml, po_ml)
 
-        except Exception:
+        except Exception as e:
             if self.env.company_id.sync_picking_failure_action == "raise":
                 raise
             else:
-                self._notify_picking_problem(purchase)
+                self._notify_picking_problem(purchase, additional_note=str(e))
 
     def _notify_picking_problem(self, purchase, additional_note=False):
         self.ensure_one()
